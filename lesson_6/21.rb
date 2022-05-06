dbd1d4743e2404e910291f953691f69bf60e8de4
Suzy Oh <suzysjoh@gmail.com>
2022-05-05 20:14:35 -0500

    21 after review

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -15,7 +15,7 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def p_d_hand(hand)
+def hands(hand)
   hand.map { |card| card[0] }
 end
 
@@ -27,7 +27,66 @@ def hand_value(hand)
 end
 
 def display_hand(hand)
-  p_d_hand(hand).join(" & ")
+  hands(hand).join(" & ")
+end
+# -------- methods for instructions------------
+
+def objective
+  <<~EXPLAIN
+    =======================================================================
+                                    OBJECTIVE
+    =======================================================================
+    To win the game, one must obtain or get closest to 21 without going over.
+    If you go over 21, you will automatically lose, known as a "Bust."
+    Both you and the dealer will start with two cards.
+    As the Player, you will get the first turn.
+    You can 'Hit' to add another card to get the value closer to 21 or
+    'Stay' to maintain the hand you have.
+    Once you 'Stay,' it will be the dealer's turn.
+    The dealer will also hit or stay depending on their hand.
+    Dealer MUST hit if their hand is under 17, and stay if greater than 17.
+
+    => Press any key to continue...
+    EXPLAIN
+end
+
+def card_value_instructions
+  <<~EXPLAIN
+    =======================================================================
+                                   CARD VALUES
+    =======================================================================
+    NUMBER CARDS are their number's value. Number cards include 1-10.
+    e.g. 4 of Hearts card's value is 4. \n\n
+    FACE CARDS have the value of 10. Face cards include Jack, Queen and King.
+    e.g. Jack of Spades card's value is 10 \n\n
+    ACE CARDS can have the value of 11 or 1.
+    e.g. Ace of Diamonds and Queen of Clubs will have the total value of 21.
+    e.g. Ace of Diamonds and Ace of Spades will have the total value of 12.
+      If a hand with an Ace exceeds 21, then the Ace card will take the value of 1. \n\n
+    => Press any key to start the game!
+  EXPLAIN
+end
+# ------- end method for instructions----------------
+
+def instructions
+  prompt "Would you like a tutorial on how to play 21?"
+  prompt "press any key for tutorial or type 'n' for no"
+  instruct = gets.chomp
+  system 'clear'
+  loop do
+    break if instruct.start_with?('n')
+    puts objective
+    continue = gets.chomp
+    break if continue
+  end
+  system 'clear'
+    loop do
+      break if instruct.start_with?('n')
+      puts card_value_instructions
+      continue = gets.chomp
+      break if continue
+    end
+  system 'clear'
 end
 
 def summary_of_hand(hand)
@@ -45,30 +104,25 @@ def remove_hands_from_deck(hand, deck)
   end
 end
 
-def stay(hand)
-  prompt "Your cards are: #{display_hand(hand)}"
-  case hand_value(hand)
-  when 0..20
-    prompt "for a sum of #{hand_value(hand)}"
-  when 21
-    prompt "CONGRATS, YOU HAVE 21! You must stay."
-  end
+def pick_cards(hand, deck)
+  hit(hand, deck)
+  remove_hands_from_deck(hand, deck)
 end
 
-def dealer_hit_or_stay(hand, deck)
+def dealer_turn(hand, deck)
   loop do
     case hand_value(hand)
     when (17..21)
       prompt "Dealer will stay"
     when (0..16)
       puts "Dealer will hit."
-      hit(hand, deck)
-      remove_hands_from_deck(hand, deck)
+      pick_cards(hand, deck)
     end
     break if hand_value(hand) >= 17
   end
 end
 
+# Some logic for game_eval method -------------------------------------------
 def winning_play?(player, dealer)
   hand_value(player) == 21 && hand_value(dealer) != 21 ||
     hand_value(player) < 21 && hand_value(player) > hand_value(dealer)
@@ -87,6 +141,7 @@ end
 def busted?(cards)
   hand_value(cards) > 21
 end
+# End of game_eval logics --------------------------------------------------
 
 def game_eval(player, dealer)
   if busted?(player)
@@ -102,6 +157,34 @@ def game_eval(player, dealer)
   end
 end
 
+def game_summary(player, dealer)
+  puts "============================================================"
+  game_eval(player, dealer)
+  display = <<~DISP
+    You have played #{display_hand(player)}
+    for a total of #{hand_value(player)} \n
+    Dealer played #{display_hand(dealer)}
+    for a total of #{hand_value(dealer)}
+    ============================================================
+    DISP
+  puts display
+end
+
+def hit_sequence(player, deck)
+  hit(player, deck)
+  remove_hands_from_deck(player, deck)
+  summary_of_hand(player)
+end
+
+def invalid_hit_stay_response
+  message = <<~MSG
+    That is an invalid input
+    Type 'h' or 'hit' to Hit
+    's' or 'stay' to Stay.
+  MSG
+  puts message
+end
+
 def prompt_player_twenty_one
   puts "======================================"
   prompt "CONGRATS! You hit 21. You must stay."
@@ -112,28 +195,28 @@ end
 
 player_win = []
 dealer_win = []
-
+loop_counter = 0
 loop do
   deck = initialize_deck
   player_cards = []
   dealer_cards = []
-  player_cards = deck.sample, deck.sample
-  remove_hands_from_deck(player_cards, deck)
-  dealer_cards = deck.sample, deck.sample
-  remove_hands_from_deck(dealer_cards, deck)
-  dealer_total = hand_value(dealer_cards)
-  player_total = hand_value(player_cards)
+  2.times do
+    pick_cards(player_cards, deck)
+    pick_cards(dealer_cards, deck)
+  end
 
   system 'clear'
-  prompt "Welcome to 21!"
-  prompt "You will start with two cards: "
-  prompt display_hand(player_cards)
-  prompt "Dealer also has two cards:"
-  prompt "#{p_d_hand(dealer_cards)[0]} & unknown card"
+  if loop_counter < 1
+    prompt "Welcome to 21!\n\n"
+    instructions
+  end
+  prompt "Your cards:"
+  prompt "#{display_hand(player_cards)}\n\n"
+  prompt "Dealer's cards:"
+  prompt "#{hands(dealer_cards)[0]} & unknown card\n\n"
 
   loop do
     player_total = hand_value(player_cards)
-
     case player_total
     when (0..20)
       prompt "Would you like to hit or stay?"
@@ -145,32 +228,17 @@ loop do
 
     if input.start_with?('h')
       system 'clear'
-      hit(player_cards, deck)
-      player_total = hand_value(player_cards)
-      remove_hands_from_deck(player_cards, deck)
-      summary_of_hand(player_cards)
-      break if player_total > 21
+      hit_sequence(player_cards, deck)
+      break if hand_value(player_cards) > 21
     elsif input.start_with?('s')
-      dealer_hit_or_stay(dealer_cards, deck)
-      dealer_total = hand_value(dealer_cards)
+      dealer_turn(dealer_cards, deck)
       break
     else
-      prompt "That is an invalid input"
-      prompt "Type 'h' or 'hit' to Hit"
-      prompt "'s' or 'stay' to Stay. "
+      invalid_hit_stay_response
     end
   end
 
-  player_total = hand_value(player_cards)
-  dealer_total = hand_value(dealer_cards)
-
-  puts "========================================================="
-  game_eval(player_cards, dealer_cards)
-  prompt "You have played #{display_hand(player_cards)}"
-  prompt "for a total of #{player_total}"
-  prompt "Dealer played #{display_hand(dealer_cards)}"
-  prompt "for a total of #{dealer_total}"
-  puts "========================================================="
+  puts game_summary(player_cards, dealer_cards)
 
   if winning_play?(player_cards, dealer_cards) || busted?(dealer_cards)
     player_win << 1
@@ -189,6 +257,8 @@ loop do
   end
 
   prompt "Would you like to go again?"
+  loop_counter += 1
   end_response = gets.chomp
+  prompt "Thanks for playing 21!" if end_response.start_with?("n")
   break if end_response.start_with?("n")
 end
